bea9f40d42013271e1306a7c8b420e3a02fd01bf
alex-odhis <lxodhiambo@gmail.com>
2022-04-09 01:36:28 +0000

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digit number from 0
+ * Return: 0 success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 59; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
