4b156cbc10c2d0dc41956c58dba9ae89f7e4d0e2
alex-odhis <lxodhiambo@gmail.com>
2022-04-09 10:31:30 +0000

    base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints basse 1o numbers to console
+ * Return: 0 success
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number <= 10; number++)
+	{
+		printf(number);
+	}
+	putchar('\n');
+	return (0);
+}
