99957b49cea7c53f2634a6e315f7996cb3fa973b
alex-odhis <lxodhiambo@gmail.com>
2022-04-09 00:44:35 +0000

    print base16 numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints numbers of base 16 followed by newline
+ * Return: 0 always
+ */
+int main(void)
+{
+	int n;
+	char ch;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		for (ch < 'a'; ch < f; ch++)
+		{
+			putchar(ch)
+		}
+		putchar('\n');
+		return (0);
+	}
+}
