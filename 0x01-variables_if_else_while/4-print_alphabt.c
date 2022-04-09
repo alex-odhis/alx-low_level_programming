e87239f84c0a3373058b1b091bccd72194b72b6a
alex-odhis <lxodhiambo@gmail.com>
2022-04-09 02:01:03 +0000

    output all alpahabet ecept q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints the laters of the alphabet
+ * Return: 0 success
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+		{
+			putchar(letter);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
