979dd260d3a550e5a3be9e1753ebdbe747a8b67a
alex-odhis <lxodhiambo@gmail.com>
2022-04-11 14:16:18 +0000

    correcting number 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	int number;
 
-	for (number = 0; number <= 10; number++)
+	for (number = 0; number <= 9; number++)
 	{
-		printf(number);
+		putchar(number);
 	}
 	putchar('\n');
 	return (0);
