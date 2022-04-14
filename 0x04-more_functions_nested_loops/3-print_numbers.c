f948e7c519a5c93e38f6172815ea5f400299ad64
alex-odhis <lxodhiambo@gmail.com>
2022-04-14 22:40:49 +0000

    program that displays digitd

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_numbers - a function that prints the numbers, from 0 to 9,
+ *followed by a new line.
+ *Return: Always 0
+ */
+void print_numbers(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
